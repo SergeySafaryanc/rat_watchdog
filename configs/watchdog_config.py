df492e2ab1484fb7b994ccb304a560d29b9c71f0
JackHuman <maxtereshenko@gmail.com>
2020-09-16 16:59:00 +0300

    comments

--- configs/watchdog_config.py
+++ configs/watchdog_config.py
@@ -6,51 +6,51 @@ PORT = 5000
 # В директорию out_path выводится файл:
 # dat inf с названием train или test.py + время начала записи
 # файлы с результатами имеют такое же название как и у dat файла с префиксами: result, _validation_result, _responses_classifiers
-wait_time = 1  # sec
-epoch_time = 0.04  # sec
-inp_path = "/home/maxburbelov/plexon/input"
-out_path = "/home/maxburbelov/plexon/output"
+wait_time = 1  # sec # ожидание детекта
+epoch_time = 0.04  # sec # время перерыва чтения из файла
+inp_path = "C:\\Users\\JackHuman\\WatchdogFiles\\input"
+out_path = "C:\\Users\\JackHuman\\WatchdogFiles\\output"
 
-sampling_rate = 1000
-decimate_rate = 10
-num_channels = 18
+sampling_rate = 1000  # частота дискретизации
+decimate_rate = 10  # прорежение данных
+num_channels = 17  # число каналов ЭГ+дыхание+метка
 
-count_train_stimuls = 100
-train_step = 25
+count_train_stimuls = 100  # число тренировок
+train_step = 25  # число смещений
 
-data_source_is_file = True
-is_result_validation = True
+data_source_is_file = True  # выбор датасорса, true - file, false - socket
+is_result_validation = True  # не трогать (типа валидация, кол-во правильных из скольких)
 
-is_train = True
-use_auto_train = True
+is_train = True  # обучение или тестирование
+use_auto_train = True  # обучение сразу после 100-ой подачи (не трограть)
 
 
-odors = [("Взрывчатое", "#ffff00"), ("Наркотическое", "#ffff00"), ("Иное", "#ffff00"), ("4", "#ffff00"),
-         ("5", "#ffff00"), ("3", "#ffff00")]
+odors = [("ТНТ", "#ffff00"), ("КП НП", "#ffff00"), ("Воздух из баллона", "#ffff00"), ("ТНТ + КП НП", "#ffff00"),
+         ("5", "#ffff00"), ("3", "#ffff00")]  # Расшифровка и цвет вещества (на экран и в таблицу)
 
-odors_set = [1, 2, 4, 8, 16]
+odors_set = [1, 8, 16, 32]  # метки веществ (числовые)
 
-unite = [[1, 2, 4], [8, 16]]
+unite = [[1], [8], [16], [32]]  # передаётся в классификатор для обучения
 
-unite_test = [[1, 2, 4], [8, 16]]  # Для подсчета результатов при смене клапанов
+unite_test = [[1], [8], [16], [32]]  # для подсчета результатов при смене веществ на клапанах
 
-odors_true = [0, 1, 2, 3, 4]
+odors_true = [0, 1, 2, 3, 4]  # не используется
 
-result_messages = [("Ожидание", "#CCC")]
-show_result_delay = 2  # 20 # sec
+result_messages = [("Ожидание", "#CCC")]  # сообщения, выводимые в UI
+show_result_delay = 2  # 20 # sec # сколько будет висеть метка
 
 channel_pairs_to_corr = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [0, 10], [0, 11],
-                         [0, 12]]
-channel_pairs_to_corr_means = True
+                         [0, 12]]  # корреляция между каналами (усреднённая, не трогать)
+channel_pairs_to_corr_means = True  # не используется
 
-corr_len = 30000
+corr_len = 30000  # число отчётов, через которое и на основании которых обновится график
 
-pylib_path = r'../odor_classifier'
+pylib_path = r'../odor_classifier'  # путь к классификатору
 
 clapan_length = 5000  # длина стимула
 prestimul_length = 3000  # длина предстимульного отрезка в файле
-stimul_delay = 0  # за держка на стимуле. Если пердстимульный отрезок и стимул одинаковые, то =0 else stimul_delay=clapan_length-prestimul_length
-num_of_channels = 16  # количество каналав ЭОГ
+stimul_delay = 0  # задержка на стимуле. Если пердстимульный отрезок и стимул одинаковые, то =0 else stimul_delay=clapan_length-prestimul_length
+num_of_channels = 15  # количество каналав ЭОГ
 air_clapan = False  # наличие в файлет на последнем канале метки открытия клапана воздуха
 using_air_clapan = False  # использовать клапан воздуха для классификации как стимул
 mixture_groups = [[1], [2], [3], [4], [5]]  # для классификации по клапанам
