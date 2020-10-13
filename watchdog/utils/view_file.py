197a718ce33be9880a542c96dc4a6edaf3d4f530
jackhuman <maxtereshenko@gmail.com>
2020-10-13 09:07:03 +0300

    abstract config

--- watchdog/utils/view_file.py
+++ watchdog/utils/view_file.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 N_CHANNELS = 18
-f = np.fromfile("/home/quantum/PycharmProjects/rat_watchdog/data/inp/_.dat", "i2")
+f = np.fromfile("/home/jackhuman/PycharmProjects/WatchdogFiles/input/08.10.2020.FI118.2.dat", "i2")
 print(f.shape)
 data = np.reshape(f, (-1, N_CHANNELS))
 # fig, axes = plt.subplots(N_CHANNELS, 1, figsize=(16, 8));
